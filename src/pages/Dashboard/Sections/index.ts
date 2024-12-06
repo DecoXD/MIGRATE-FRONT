6e2a1fb4eabcb974c9241d80d436f247be94ed41
unknown <deco.xd@outlook.com>
2024-12-06 10:13:52 -0300

    addes settings section in dashboard'

--- src/pages/Dashboard/Sections/index.ts
+++ src/pages/Dashboard/Sections/index.ts
@@ -1,7 +1,10 @@
 import DashboardIndex from "./DashboardIndex";
 import Notifications from "./Notifications";
 import Users from "./Users";
+import Products from "./Products";
+import Orders from "./Orders";
+import Settings from "./Settings";
 
 
 
-export{DashboardIndex,Notifications,Users}
+export{DashboardIndex,Notifications,Users,Orders,Products,Settings}
